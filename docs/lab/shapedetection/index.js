521d3df04b91cf684c4206e79935abbb5a7f87a3
jack-8 <dorayaki08080@gmail.com>
2021-10-02 02:15:12 +0900

    update shape detection sample

--- docs/lab/shapedetection/index.js
+++ docs/lab/shapedetection/index.js
@@ -1,5 +1,6 @@
 // https://github.com/WICG/shape-detection-api
 // https://wicg.github.io/shape-detection-api
+// https://web.dev/shape-detection/
 
 const detect = async (image) => {
   const supported = await (
@@ -20,13 +21,49 @@ const detect = async (image) => {
     if (image) {
       const worker = new Worker(new URL('worker.js', import.meta.url), { type: 'module' });
       worker.postMessage({ qrcode: image });
+
+      worker.addEventListener('message', (event) => {
+        if (event.data) {
+          console.debug('Message received from worker: ', event.data);
+        }
+      });
     }
   } else {
     alert('Your browser doesn\'t support!');
   }
 }
 
-// todo カメラからQRコード取得
+const cameraLaunch = document.getElementById('launch');
+cameraLaunch.addEventListener('click', async (e) =>{
+  const stream = await navigator.mediaDevices.getUserMedia({
+    audio: false,
+    video: {
+      // facingMode: { exact: "environment" },
+      facingMode: "user",
+      width: 300,
+      height: 300
+    }
+  });
 
-detect();
+  const video = document.createElement('video');
+  video.srcObject = stream;
 
+  document.body.appendChild(video);
+  video.play();
+
+  setTimeout( async () => {
+    // ビデオ停止
+    video.pause();
+    // 画像生成
+    const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d');
+    const w = video.offsetWidth;
+    const h = video.offsetHeight;
+    canvas.setAttribute('width', w);
+    canvas.setAttribute('height', h);
+    ctx.drawImage(video, 0, 0, w, h);
+    stream.getTracks()[0].stop();
+    // QR読み込み
+    detect(ctx.getImageData(0, 0, canvas.width, canvas.height));
+  }, 3000);
+});
