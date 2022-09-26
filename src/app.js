f6aeda1c34be076e2d9497a729264a83114edd19
jack-8 <dorayaki08080@gmail.com>
2022-09-26 13:04:33 +0900

    maintenance

--- src/app.js
+++ src/app.js
@@ -1,7 +1,17 @@
 import express from "express";
+import http from "http";
+import https from "https";
+import fs from "fs";
+
 const app = express();
-const port = 3000;
+const httpPort = 3000;
+const httpsPort = 3001;
 const router = express.Router();
+
+const options = {
+  key: fs.readFileSync("./privatekey.pem"),
+  cart: fs.readFileSync("./cert.pem")
+}
 
 router.use((req, res, next) => {
   res.set('Cache-control', 'no-store');
@@ -12,8 +22,14 @@ router.use((req, res, next) => {
 
 app.use("/monaka", router, express.static("app"));
 
-const server = app.listen(port, () => {
-  const h = server.address().address;
-  const p = server.address().port;
+const httpServer = http.createServer(app).listen(httpPort, () => {
+  const h = httpServer.address().address;
+  const p = httpServer.address().port;
   console.log(`App listening at http://${h}:${p}`);
 });
+
+// const httpsServer = https.createServer(app).listen(httpPort, () => {
+//   const h = httpsServer.address().address;
+//   const p = httpsServer.address().port;
+//   console.log(`App listening at https://${h}:${p}`);
+// });
