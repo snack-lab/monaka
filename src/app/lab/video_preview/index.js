e314a6e21d47d049bce8f6a654c101608b4db175
jack-8 <dorayaki08080@gmail.com>
2022-09-29 08:38:15 +0900

    maintenance

--- src/app/lab/video_preview/index.js
+++ src/app/lab/video_preview/index.js
@@ -1,10 +1,46 @@
-window.addEventListener('load', (e) => {
-  const videoFile = document.getElementById('videoUpload');
-  videoFile.addEventListener('change', (e) => {
-    const file = e.target.files[0];
-    const blob = URL.createObjectURL(file);
+const videoFile = document.getElementById('videoUpload');
 
-    const video = document.querySelector("video");
-    video.src = blob;
-  })
-})
+const handleFiles = (event) => {
+  console.debug(event.type, event);
+
+  this.#root.querySelector(".preview video")?.remove();
+
+  if (event.target.files.length <= 0) return;
+
+  const arrayFileList = Array.from(event.target.files);
+  const mimeTypes = ["video/mp4"];
+  const sizeLimit = 1024 * 1024 * 1024 * 1;
+
+  // check mimeType
+  const notVideos = arrayFileList.filter(file => !mimeTypes.includes(file.type));
+  if (0 < notVideos.length) {
+      event.target.value = null;
+      return;
+  }
+
+  // check size
+  const sizeOverVideos = arrayFileList.filter(file => (file.size <= 0 || sizeLimit < file.size));
+  if (0 < sizeOverVideos.length) {
+      event.target.value = null;
+      return;
+  }
+
+  const newVideo = document.createElement('video');
+  newVideo.controls = true;
+  newVideo.width = 350;
+
+  const blob = new Blob(event.target.files, { type: mimeTypes[0] });
+  if ('srcObject' in newVideo) {
+      try {
+          newVideo.srcObject = blob;
+      } catch (err) {
+          newVideo.src = URL.createObjectURL(blob);
+      }
+  } else {
+      newVideo.src = URL.createObjectURLblob(blob);
+  }
+
+  this.#root.querySelector(".preview").appendChild(newVideo);
+}
+
+videoFile.onchange = handleFiles;
