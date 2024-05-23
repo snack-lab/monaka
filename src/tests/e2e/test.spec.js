29fa3c35f60f9b1a4b6a8a22d38406cf457c0115
Ym <dorayaki08080@gmail.com>
2024-05-24 01:12:45 +0900

    add playwright

---
+++ src/tests/e2e/test.spec.js
@@ -0,0 +1,12 @@
+// https://playwright.dev/
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('http://localhost:3000/monaka/index.html');
+  await expect(page).toHaveTitle(/monaka/);
+});
+
+// test('Page Screenshot', async ({ page }) => {
+//   await page.goto('http://localhost:3000/monaka/index.html');
+//   await page.screenshot({ path: `example.png` });
+// });
