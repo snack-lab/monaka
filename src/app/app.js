b3af9d346ef617c18787102267c2d1e9512db07f
Ym <dorayaki08080@gmail.com>
2024-07-20 00:21:25 +0900

    add standalone title

--- src/app/app.js
+++ src/app/app.js
@@ -1,3 +1,8 @@
 import "./js/worker/swRegister.js";
 import "./js/observer/report.js";
 // import "./js/navigation.js";
+
+const std = window.matchMedia('(display-mode: standalone)');
+if(std.matches) {
+    document.title = `${document.title} (app)`;
+}
