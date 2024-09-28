deb88ae9a23369e7738dccffbf3efb513d0ed10f
yw <dorayaki08080@gmail.com>
2024-09-29 02:24:38 +0900

    udpate dcocs

--- docs/mosha/webapi/virtual_keyboard/index.js
+++ docs/mosha/webapi/virtual_keyboard/index.js
@@ -7,8 +7,6 @@ if ('virtualKeyboard' in navigator) {
     console.log("virtual keyboard show");
     // 仮想キーボード表示
     navigator.virtualKeyboard.show();
-    vEdit.classList.add('some-class');
-    vEdit.classList.remove('some-other-class');
 
     // 仮想キーボードの現在のジオメトリを取得
     const { x, y, width, height } = navigator.virtualKeyboard.boundingRect;
@@ -19,15 +17,13 @@ if ('virtualKeyboard' in navigator) {
     console.log("virtual keyboard hide");
     // 仮想キーボード非表示
     navigator.virtualKeyboard.hide();
-    vEdit.classList.add('some-other-class');
-    vEdit.classList.remove('some-class');
 
     // 仮想キーボードの現在のジオメトリを取得
     const { x, y, width, height } = navigator.virtualKeyboard.boundingRect;
     console.log('Virtual keyboard geometry:', x, y, width, height);
   });
 
-  navigator.virtualKeyboard.addEventListener('geometrychanged', (event) => {
+  navigator.virtualKeyboard.addEventListener('geometrychange', (event) => {
     // 仮想キーボード表示/非表示時のイベント
     const { x, y, width, height } = event.target;
     console.log('Virtual keyboard geometry changed:', x, y, width, height);
