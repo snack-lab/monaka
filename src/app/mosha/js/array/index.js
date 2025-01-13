eb6da8cde41bca5b9a6eb03b5e08d88edf347cb9
yw <dorayaki08080@gmail.com>
2025-01-13 11:02:14 +0900

    update

---
+++ src/app/mosha/js/array/index.js
@@ -0,0 +1,8 @@
+// with
+{
+  const array_base = [1, 2, 3, 4, 5, 6];
+  const array_with = array_base.with(2, 7);
+
+  console.debug("array_base", array_base);
+  console.debug("array_with", array_with);
+}
