413df606d11db2cfad07269655b131c5ded74a7c
YU <dorayaki08080@gmail.com>
2025-06-08 01:51:45 +0900

    update playwright

--- src/tests/e2e/test.spec.js
+++ src/tests/e2e/test.spec.js
@@ -1,13 +1,6 @@
-// https://playwright.dev/
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
   await page.goto('http://monaka_app:3000/monaka/index.html');
   await expect(page).toHaveTitle(/Home/);
 });
-
-// test('Page Screenshot', async ({ page }) => {
-//   await page.goto('http://localhost:3000/monaka/index.html');
-//   await page.screenshot({ path: `example.png` });
-// .toHaveScreenshot() // 前回撮影したスクリーンショットの結果と比較
-// });
